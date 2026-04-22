d904177aa37d60e475431b383be8f403bc5e87d0
Pedro Vázquez <vanstrouble@proton.me>
2026-04-22 16:03:58 -0600

    Add Task model and admin configuration for task management

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at",)
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
--- task/models.py
+++ task/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + " - by " + self.user.username
